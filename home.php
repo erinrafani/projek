fe740a176bd8883f3cdac904595681a9e75cf10b
erinrfn <90381083+erinrafani@users.noreply.github.com>
2021-09-30 22:14:27 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Perpustakaan Online</title>
+</head>
+<body>
+
+<div id="container">
+<div class="header"></div>
+</div>
+
+<center><div class="main">
+<div class="left">
+<h3 align="center">MENU</h3>
+<ul>
+<li><a href="login.php">Login</a></li>
+<li><a href="home.php">Home</a></li>
+<li><a href="perpus/index.php">Daftar Buku</a></li>
+</ul>
+</div></center>
+
+<div class="footer"><p align="center">Copyright © 2018 - Belajar CSS Responsive</a></p>
+</div>
+</div>
+</body>
+</html>
